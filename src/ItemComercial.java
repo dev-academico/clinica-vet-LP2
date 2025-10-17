86faa412aa4fa0744066ef2387ebf1d447ea9ba4
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-10-17 18:08:52 -0300

    Add new impl and functions: need a get for worker name

--- src/ItemComercial.java
+++ src/ItemComercial.java
@@ -1,40 +1,51 @@
-import java.io.InvalidClassException;
+import java.util.HashMap;
+import java.util.function.Function;
 
 abstract class ItemComercial {
     static private int contItem=0;
 
-     final private Integer id;
-     final private String nome;
-     private String desc;
+    final private Integer id;
+    final private String nome;
+    private String desc;
+    private HashMap<Integer, Funcionario> responsaveisServ;
 
-    ItemComercial(Integer id, String nome, String desc){
+    ItemComercial(Integer id, String nome, String desc, Funcionario fnr){
         this.id=id;
         this.nome=nome;
         this.desc=desc;
         this.contItem+=1;
+        this.responsaveisServ=responsaveisServ<id, fnr>;
     }
 
     //uso de polim. aq ↓
-    public static boolean aplicarDesconto(){
+    public boolean aplicarDesconto(Double v) throws DescontoInvalidoE{
         boolean apply=false;
 
-        return apply;
+           try{
+               DescontoInvalidoE.validaPercentual(v);
+               //impl logica do produto/serv. a ser descontado nas classes
+               apply=true;
+               return apply;
+           }catch(DescontoInvalidoE e){
+               System.out.println(e.getMessage());
+               return apply;
+           }
     }
 
+    //Identifica um serviço e um responsável
+    public void incluiFuncionario(Funcionario F){
+        HashMap itemC= new HashMap<this.id,F.nome>(); //Necessário um getter
+        System.out.println("Funcionário do responsável pelo serviço: "+ItemComercial.nome+" é" +
+                 F.nome);
+        //implementar: controle para identificar se é produto ou não
+    }
+    public boolean consumoItem(){
+      //mostra se um cliente consumiu um prod. ou serviço
+        boolean cons=false;
+
+        return cons;
+    }
 }
 
 //mover para uma pasta dedicada
-class DescontoInvalidoE extends Exception{
-    public DescontoInvalidoE(String msg){
-        super(msg);
-    }
-    public static void validaPercentual(double p) throws DescontoInvalidoE{
-        if(p<0){
-            throw new DescontoInvalidoE("O percentual negativo é inválido\n.");
-        }
-        if(p>0.95){
-            throw new DescontoInvalidoE("Valor de desconto está acima do limite máximo\n.");
-        }
-    }
-}
 
