b9c5d060bf2c8d179fb3e6f8f80fdd10726207f0
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-10-20 16:38:05 -0300

    Create Servico class extending ItemComercial

---
+++ src/Servico.java
@@ -0,0 +1,21 @@
+public class Servico extends ItemComercial{
+
+
+    Servico(Integer id, String nome, Funcionario fnr, String desc /* outros param */){
+        super(id, nome, desc, fnr);
+
+        /*
+        impl.
+         */
+    }
+
+    /*
++ id: int
+    + nome: String
+    + descricao: String
+    + preco: float
+    + polimorfismo -> agendar()
+    + realizarServico()
+
+     */
+}
