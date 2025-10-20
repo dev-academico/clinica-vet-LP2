663fbba450242bc7b3caa095fe32028f9814f4db
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-10-20 16:37:11 -0300

    Refactor ItemComercial class for better structure

--- src/ItemComercial.java
+++ src/ItemComercial.java
@@ -1,51 +1,67 @@
-import java.util.HashMap;
-import java.util.function.Function;
+import java.util.Map;
+import java.util.Scanner;
 
 abstract class ItemComercial {
     static private int contItem=0;
 
-    final private Integer id;
-    final private String nome;
+    private Integer id;
+    private String nome;//talvez seja melhor final
     private String desc;
-    private HashMap<Integer, Funcionario> responsaveisServ;
+    private Map<Integer, Funcionario> responsaveisServ;
 
     ItemComercial(Integer id, String nome, String desc, Funcionario fnr){
         this.id=id;
         this.nome=nome;
         this.desc=desc;
-        this.contItem+=1;
-        this.responsaveisServ=responsaveisServ<id, fnr>;
+        contItem++;
+        this.responsaveisServ.put(this.id, fnr); //cada id é mapeado com o funcionário
     }
 
     //uso de polim. aq ↓
     public boolean aplicarDesconto(Double v) throws DescontoInvalidoE{
         boolean apply=false;
 
-           try{
-               DescontoInvalidoE.validaPercentual(v);
-               //impl logica do produto/serv. a ser descontado nas classes
-               apply=true;
-               return apply;
-           }catch(DescontoInvalidoE e){
-               System.out.println(e.getMessage());
-               return apply;
-           }
+        try{
+            DescontoInvalidoE.validaPercentual(v);
+            //impl logica do produto/serv. a ser descontado nas classes
+            apply=true;
+            return apply;
+        }catch(DescontoInvalidoE e){
+            System.out.println(e.getMessage());
+            return apply;
+        }
     }
 
     //Identifica um serviço e um responsável
     public void incluiFuncionario(Funcionario F){
-        HashMap itemC= new HashMap<this.id,F.nome>(); //Necessário um getter
-        System.out.println("Funcionário do responsável pelo serviço: "+ItemComercial.nome+" é" +
-                 F.nome);
+        Map<Integer, Funcionario> itemC; // !!! Necessário um getter
+        Scanner s=new Scanner(System.in);
+        System.out.println("Informe o código do serviço");
+        id=s.nextInt();
+        /*
+        A IMPLEMENTAR: ACESSO AO NOME DO FUNCIONARIO
+
+        itemC.put(id, F.nome);
+                System.out.println("Funcionário do responsável pelo serviço: "+ItemComercial.nome+" é" +
+                F.nome);
         //implementar: controle para identificar se é produto ou não
+
+
+         */
     }
-    public boolean consumoItem(){
-      //mostra se um cliente consumiu um prod. ou serviço
+    public boolean consumoItem(Cliente cl){
+        /*
+        CRIA UM RELATÓRIO A RESPEITO DO CONSUMO DO CLIENTE
+
+        - parâmetros do 'cl': preço, data_hora, id do serviço/produto, nome do cliente, forma de pagamento
+
+         */
+
+
         boolean cons=false;
+
+
 
         return cons;
     }
 }
-
-//mover para uma pasta dedicada
-
