321f2f7d828f416615da04f48c7499408e48df79
Raquel Garcia <raqueldossantosgarcia.com>
2025-10-21 17:17:19 -0300

    tentando inserir exceções

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import exception.DadosObrigatoriosException;
 import model.Cargo;
 import model.Clinica;
 import model.Funcionario;
@@ -8,34 +9,38 @@ public class Main {
         Clinica clinica = new Clinica();
 
         //CRUD veterinário
-        Veterinario veterinario1 = new Veterinario("Raquel Garcia", "111.111.111-11", "parco della veritá", "84 98860-6745", 100000, "1010101");
-        Veterinario veterinario2 = new Veterinario("Raquel Garcia", "000.000.000-00", "parco della veritá", "84 98860-6745", 100000, "1010102");
+        try{
+            Veterinario veterinario1 = new Veterinario("Raquel Garcia", "111.111.111-11", "parco della veritá", "84 98860-6745", 100000, "1010101");
+            Veterinario veterinario2 = new Veterinario(null, "000.000.000-00", "parco della veritá", "84 98860-6745", 100000, "1010102");
 
-        //create veterinário
-        clinica.adicionarVeterinário(veterinario1);
-        clinica.adicionarVeterinário(veterinario2);
+            //create veterinário
+            clinica.adicionarVeterinário(veterinario1);
+            clinica.adicionarVeterinário(veterinario2);
 
-        System.out.println();
+            System.out.println();
 
-        //remover veterinário
-        clinica.removerVeterinário(veterinario2);
+            //remover veterinário
+            clinica.removerVeterinário(veterinario2);
 
-        System.out.println();
+            System.out.println();
 
-        // read
-        clinica.lerVeterinário("1010101");
+            // read
+            clinica.lerVeterinário("1010101");
 
-        System.out.println();
+            System.out.println();
 
-        // update veterinário
-        clinica.atualizarVeterinário(null, "111.111.111-11", null, null, 300000, "1010101");
+            // update veterinário
+            clinica.atualizarVeterinário(null, "111.111.111-11", null, null, 300000, "1010101");
 
-        System.out.println();
+            System.out.println();
 
-        // read
-        clinica.lerVeterinário("1010101");
+            // read
+            clinica.lerVeterinário("1010101");
 
-        System.out.println();
+            System.out.println();
+        }catch(DadosObrigatoriosException e){
+            System.out.println(e);
+        }
 
         //CRUD funcionário
         Funcionario funcionario1 = new Funcionario("Raquel Garcia", "111.111.111-11", "parco della veritá", "84 98860-6745", 100000, "2020202", Cargo.AUXILIAR_TOSADOR);
