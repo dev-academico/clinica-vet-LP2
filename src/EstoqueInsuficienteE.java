cde562097d6048b9ee2261823d580692ac3d2080
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-10-23 16:41:48 -0300

    Create EstoqueInsuficienteE exception class

---
+++ src/EstoqueInsuficienteE.java
@@ -0,0 +1,17 @@
+import java.io.InvalidClassException;
+
+
+public class EstoqueInsuficienteE extends Exception{
+    public EstoqueInsuficienteE(String msg){
+        super(msg);
+    }
+
+    public static void validaQtd(int etq, Integer real_etq) throws EstoqueInsuficienteE{
+
+        if(etq>real_etq){
+            throw new EstoqueInsuficienteE("O estoque é insuficiente\n.");
+        }
+
+    }
+
+}
