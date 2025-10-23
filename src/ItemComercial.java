bc6f67bd24a75e0105dd7ad40fd8555e1fe61a7f
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-10-23 16:40:57 -0300

    Refactor ItemComercial class structure
    Refactor ItemComercial class by moving responsibility mapping and changing aplicarDesconto to abstract.

--- src/ItemComercial.java
+++ src/ItemComercial.java
@@ -7,48 +7,20 @@ abstract class ItemComercial {
     private Integer id;
     private String nome;//talvez seja melhor final
     private String desc;
-    private Map<Integer, Funcionario> responsaveisServ;
 
     ItemComercial(Integer id, String nome, String desc, Funcionario fnr){
         this.id=id;
         this.nome=nome;
         this.desc=desc;
         contItem++;
-        this.responsaveisServ.put(this.id, fnr); //cada id é mapeado com o funcionário
+        //this.responsaveisServ.put(this.id, fnr); //movido para serviço
     }
 
-    //uso de polim. aq ↓
-    public boolean aplicarDesconto(Double v) throws DescontoInvalidoE{
-        boolean apply=false;
-
-        try{
-            DescontoInvalidoE.validaPercentual(v);
-            //impl logica do produto/serv. a ser descontado nas classes
-            apply=true;
-            return apply;
-        }catch(DescontoInvalidoE e){
-            System.out.println(e.getMessage());
-            return apply;
-        }
-    }
+    //ovr
+    public abstract boolean aplicarDesconto(Double v) throws DescontoInvalidoE;
 
     //Identifica um serviço e um responsável
-    public void incluiFuncionario(Funcionario F){
-        Map<Integer, Funcionario> itemC; // !!! Necessário um getter
-        Scanner s=new Scanner(System.in);
-        System.out.println("Informe o código do serviço");
-        id=s.nextInt();
-        /*
-        A IMPLEMENTAR: ACESSO AO NOME DO FUNCIONARIO
 
-        itemC.put(id, F.nome);
-                System.out.println("Funcionário do responsável pelo serviço: "+ItemComercial.nome+" é" +
-                F.nome);
-        //implementar: controle para identificar se é produto ou não
-
-
-         */
-    }
     public boolean consumoItem(Cliente cl){
         /*
         CRIA UM RELATÓRIO A RESPEITO DO CONSUMO DO CLIENTE
@@ -57,10 +29,7 @@ abstract class ItemComercial {
 
          */
 
-
         boolean cons=false;
-
-
 
         return cons;
     }
