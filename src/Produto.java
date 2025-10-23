0c7f2a37f197adfeeebd9b7dbc25ea5586aa933e
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-10-23 16:41:20 -0300

    Add discount application method to Produto class
    Implemented discount application logic in Produto class.

--- src/Produto.java
+++ src/Produto.java
@@ -4,7 +4,6 @@ public class Produto extends ItemComercial{
     private Float preco;
     private Integer estoque;
 
-
     Produto(Integer id, String nome, Funcionario fnr, String desc, Float p, Integer etq){
         super(id, nome, desc, fnr);
         //this.desc=desc;
@@ -12,13 +11,44 @@ public class Produto extends ItemComercial{
         this.estoque=etq;
     }
 
+    @Override
+    public boolean aplicarDesconto(Double v) throws DescontoInvalidoE{
+        boolean apply=false;
+
+             try{
+                DescontoInvalidoE.validaPercentual(v);
+                float v_f=v.floatValue(); //conversão
+                Float preco_novo=v_f*preco;
+
+                System.out.print("Desconto aplicado: "+v_f*100+"% |\nPreço a pagar (não inclui taxas): "+preco_novo);
+                System.out.println("\n");
+                apply=true;
+                return apply;
+            }catch(DescontoInvalidoE e){
+                System.out.println(e.getMessage());
+                return apply;
+            }
+        }
+
+    public Integer getEstoque(){
+        return this.estoque;
+    }
+
+    public void setEstoque(Integer val){
+        this.estoque=val;
+    }
+
+    /*
     public void vender(){
-        if(estoque<0){
+        if(estoque<0 ){
             //impl.
         }
     }
 
-    /*
+    ###########
+
+
+
     a impl
     + vender()
     + atualizarEstoque()
