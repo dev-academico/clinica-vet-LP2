13f155efb585a389ac370dea3dc2b65c92b90c82
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-10-27 04:47:57 +0000

    add new functions exception produto

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,30 +1,40 @@
+package model;
+import exception.DescontoInvalidoException;
+import exception.ProdutoNaoEncontradoException;
+
+import java.util.ArrayList;
+
 public class Produto extends ItemComercial{
 
-    private String desc;
     private Float preco;
     private Integer estoque;
+    //private ArrayList<Cliente> consumo_Pdt;
+    private ArrayList<Produto> lista_prodDisponivel;
 
     Produto(Integer id, String nome, Funcionario fnr, String desc, Float p, Integer etq){
         super(id, nome, desc, fnr);
-        //this.desc=desc;
         this.preco=p;
         this.estoque=etq;
     }
 
+    public void produtosEmLista(Produto p){
+        this.lista_prodDisponivel.add(p);
+    }
+
     @Override
-    public boolean aplicarDesconto(Double v) throws DescontoInvalidoE{
+    public boolean aplicarDesconto(Double v) {
         boolean apply=false;
 
              try{
-                DescontoInvalidoE.validaPercentual(v);
+                DescontoInvalidoException.validaPercentual(v);
                 float v_f=v.floatValue(); //conversão
-                Float preco_novo=v_f*preco;
+                float preco_novo=v_f*preco;
 
                 System.out.print("Desconto aplicado: "+v_f*100+"% |\nPreço a pagar (não inclui taxas): "+preco_novo);
                 System.out.println("\n");
                 apply=true;
                 return apply;
-            }catch(DescontoInvalidoE e){
+            }catch(DescontoInvalidoException e){
                 System.out.println(e.getMessage());
                 return apply;
             }
@@ -38,20 +48,21 @@ public class Produto extends ItemComercial{
         this.estoque=val;
     }
 
-    /*
-    public void vender(){
+  
+    public boolean vender(Produto p){
+      try{
+        ProdutoNaoEncontradoException.validaProdLista(p);
+
+        //a impl.
+
         if(estoque<0 ){
-            //impl.
+            p.estoque--;
+            return true;
+        }
+        }catch(ProdutoNaoEncontradoException e){
+            System.out.println(e.getMessage());
+            return false;
         }
     }
-
-    ###########
-
-
-
-    a impl
-    + vender()
-    + atualizarEstoque()
-
-     */
+   
 }
--- src/model/Servico.java
+++ src/model/Servico.java
@@ -1,37 +1,40 @@
+package model;
+
+import exception.DescontoInvalidoException;
+
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Servico extends ItemComercial{
 
-    private String desc;
     private Float preco;
     private Map<Integer, Funcionario> responsaveisServ;
+    //private ArrayList<Cliente> consumo_Srv; //clientes que consumiram serviço
 
     Servico(Integer id, String nome, Funcionario fnr, String desc, Float p){
         super(id, nome, desc, fnr);
-        this.desc=desc;
         this.preco=p;
     }
 
     @Override
-    public boolean aplicarDesconto(Double v) throws DescontoInvalidoE{
-    boolean apply=false;
+    public boolean aplicarDesconto(Double v){
+        boolean apply=false;
 
          try{
-             DescontoInvalidoE.validaPercentual(v);
+             DescontoInvalidoException.validaPercentual(v);
             float v_f=v.floatValue(); //conversão
-            Float preco_novo=v_f*preco;
+            float preco_novo=v_f*preco;
 
             System.out.print("Desconto aplicado: "+v_f*100+"% |\nPreço a pagar (não inclui taxas): "+preco_novo);
             System.out.println("\n");
             apply=true;
             return apply;
-        }catch(DescontoInvalidoE e){
+        }catch(DescontoInvalidoException e){
             System.out.println(e.getMessage());
             return apply;
         }
     }
-
 
     public void incluiFuncionario(Funcionario F){
         Scanner s=new Scanner(System.in);
@@ -46,10 +49,23 @@ public class Servico extends ItemComercial{
                 F.getNome());
     }
 
+    //ovr
+    public boolean agendar(Cliente cl, LocalDateTime data, itemComercial item){}
+
+    public void realizarServico(Servico svc, Cliente cl, LocalDateTime data, itemComercial item){
+        if(!svc.agendar(cl, data, item) || svc==null){
+            System.err.println("Serviço indisponível.\n");
+            return;
+        }
+
+        System.out.println("\n\nServiço a ser realizado: "+svc.getNome());
+        System.out.println("\n\nProfissional: "+responsaveisServ.get(Funcionario));
+        System.out.println("\n\nCliente em atendimento: "+cl.getNome());
+    }
 
     /*
-    + polimorfismo -> agendar()
-    + realizarServico()
-
+    public void registroConsumo(Cliente cl, LocalDateTime data, String pagForma){
+        String nome=cl.getNome();
+    }
      */
 }
